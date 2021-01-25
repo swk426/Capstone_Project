709433d1974a1dc163235f817a2581982ba9c91e
Tydus Kim <swk426@gmail.com>
2021-01-24 20:33:38 -0800

    final draft

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Building a machine learning g model that predicts  malignant skin cancer vs. benign with given picture image',
+    author='Tydus_Kim',
+    license='BSD-3',
+)
